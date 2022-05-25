1b28a61839340af057b98fbaa4615e27090f0b3f
Oswaldo Díaz <35883410+edgarOswaldoDiaz@users.noreply.github.com>
2022-05-24 19:15:54 -0500

    Add files via upload

---
+++ nifi/nifi.sh
@@ -0,0 +1,6 @@
+docker run --name nifi \
+  -p 8443:8443 \
+  -d \
+  -e SINGLE_USER_CREDENTIALS_USERNAME=admin \
+  -e SINGLE_USER_CREDENTIALS_PASSWORD=ctsBtRBKHRAx69EqUghvvgEvjnaLjFEB \
+  apache/nifi:latest
